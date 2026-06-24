e496b276811c199b24c7ea299e318e20d1c49c09
Caiissa <ralfsxhmidt.info@gmx.de>
2026-06-24 16:45:31 +0200

    Added Enclosure

---
+++ src/main/java/zoo/enclosure/Enclosure.java
@@ -0,0 +1,31 @@
+package zoo.enclosure;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import zoo.animal.Animal;
+
+public class Enclosure<T extends Animal> {
+
+  private final String name;
+
+  // Es soll keine Duplikate haben und die Reinfolge ist nicht wichtig, deswegen habe ich mich für
+  // eine HashSet entschieden
+  private final Set<T> inhabitants = new HashSet<>();
+
+  public Enclosure(String name) {
+    this.name = name;
+  }
+
+  public boolean add(T animal) {
+    return inhabitants.add(animal);
+  }
+
+  public boolean remove(T animal) {
+    return inhabitants.remove(animal);
+  }
+
+  public List<T> getInhabitants() {
+    return List.copyOf(inhabitants);
+  }
+}
